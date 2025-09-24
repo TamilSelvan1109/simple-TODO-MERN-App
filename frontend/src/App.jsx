3db02594950af80479773e3570c23d181314d050
TamilSelvan1109 <tamilselvan11092003@gmail.com>
2025-09-24 18:30:07 +0530

    post and fetch done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,110 @@
 import { useState } from "react";
+import { IoClose } from "react-icons/io5";
+import { MdModeEditOutline, MdOutlineDone } from "react-icons/md";
+// import { IoClipboardOutline } from "react-icons/io5";
+import axios from "axios";
+import { useEffect } from "react";
+import { FaTrash } from "react-icons/fa6";
 
 function App() {
   const [newTodo, setNewTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [editingTodo, setEditingTodo] = useState(null);
+  const [editedText, setEditedText] = useState("");
+  const addTodo = async (e) => {
+    e.preventDefault();
+    if (!newTodo.trim()) return;
+    try {
+      const response = await axios.post("/api/todos", { text: newTodo });
+      setTodos([...todos, response.data]);
+      setNewTodo("");
+      console.log("Todo added Successfully!");
+    } catch (error) {
+      console.log("Error in adding the todo!", error);
+    }
+  };
+
+  const fetchTodos = async () => {
+    try {
+      const response = await axios.get("/api/todos");
+      setTodos(response.data);
+      console.log(response.data);
+    } catch (error) {
+      console.log("Error in fetching todos!", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+
+  const startEditing = (todo) => {
+    setEditingTodo(todo._id);
+    setEditedText(todo.text);
+  };
+
   return (
     <div className="min-h-screen bg-gradiant-to-br from gray-50 to-gray-100 flex items-center justify-center p-4">
       <div className="bg-white rounded-2xl shadow-xl w-full max-w-lg p-8">
-        <h1 className="text-4xl font-bold text-gray-800 nb-8>Task Manager</h1>
-
-        <form action="">
+        <h1 className="text-4xl font-bold text-gray-800 mb-8">Task Manager</h1>
+        <form
+          onSubmit={addTodo}
+          className="flex items-center gap-2 shadow-sm border border-gray-200 p-2 rounded-lg"
+        >
           <input
+            className="flex-1 outline-none px-3 py-2 text-gray-700 placeholder-gray-400"
             type="text"
             value={newTodo}
             onChange={(e) => setNewTodo(e.target.value)}
             placeholder="What need to be done?"
+            required
           />
+          <button
+            type="submit"
+            className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-md font-medium cursor-point"
+          >
+            Add Task
+          </button>
         </form>
+        <div>
+          {todos.length === 0 ? (
+            <div></div>
+          ) : (
+            <div>
+              {todos.map((todo) => (
+                <div key={todo._id}>
+                  {editingTodo === todo._id ? (
+                    <div>
+                      <input
+                        type="text"
+                        value={editedText}
+                        onChange={(e) => setEditedText(e.target.value)}
+                      />
+                      <button>
+                        <MdOutlineDone />
+                      </button>
+                      <button onClick={() => setEditingTodo(null)}>
+                        <IoClose />
+                      </button>
+                    </div>
+                  ) : (
+                    <div>
+                      <div>
+                        {todo.text}
+                        <button onClick={() => startEditing(todo)}>
+                          <MdModeEditOutline />
+                        </button>
+                        <button>
+                          <FaTrash />
+                        </button>
+                      </div>
+                    </div>
+                  )}
+                </div>
+              ))}
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
